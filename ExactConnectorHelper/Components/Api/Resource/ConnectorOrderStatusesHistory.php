47ebd1477b7fcb6f9f4daae8dca80f698a149d24
Alaa Semsemea <a.semsemea@linq5.nl>
2020-12-24 12:28:27 +0100

    Resolve issue 16

---
+++ ExactConnectorHelper/Components/Api/Resource/ConnectorOrderStatusesHistory.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace ExactConnectorHelper\Components\Api\Resource;
+
+use Shopware\Components\Api\Resource\Resource;
+use Shopware\Models\Order\History;
+use Shopware\Models\Order\Status;
+use Shopware\Components\Api\Exception as ApiException;
+
+class ConnectorOrderStatusesHistory extends Resource
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository|\Shopware\Models\Dispatch\Repository
+     */
+    public function getRepository()
+    {
+        return $this->getManager()->getRepository(History::class);
+    }
+
+    /**
+     * Get order History Data
+     * @param array $criteria
+     * @param array $orderBy
+     * @return array
+     */
+    public function getList(array $criteria = [], array $orderBy = [])
+    {
+        $builder = $this->getRepository()->createQueryBuilder('history');
+
+        // Create builder on requested filter and sort
+        $builder->addFilter($criteria)
+            ->addOrderBy($orderBy);
+
+        $query = $builder->getQuery();
+        $query->setHydrationMode($this->resultMode);
+
+        $paginator = $this->getManager()->createPaginator($query);
+
+        //returns the total count of the query
+        $totalResult = $paginator->count();
+
+        //returns the order history data
+        $orderStatuses = $paginator->getIterator()->getArrayCopy();
+
+        return ['data' => $orderStatuses, 'total' => $totalResult];
+    }
+
+
+    /**
+     * Get one order status
+     *
+     * @param $id
+     * @return mixed
+     * @throws ApiException\NotFoundException
+     * @throws ApiException\ParameterMissingException
+     * @throws ApiException\PrivilegeException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getOne($id)
+    {
+        $this->checkPrivilege('read');
+
+        if (empty($id)) {
+            throw new ApiException\ParameterMissingException();
+        }
+
+        $builder = $this->getRepository()
+            ->createQueryBuilder('orderStatus')
+            ->select('orderStatus')
+            ->where('orderStatus.id =?1')
+            ->setParameter(1, $id);
+
+        $orderStatus = $builder->getQuery()->getOneOrNullResult($this->getResultMode());
+
+        if (!$orderStatus) {
+            throw new ApiException\NotFoundException("Order status by id $id not found");
+        }
+
+        return $orderStatus;
+    }
+
+}
